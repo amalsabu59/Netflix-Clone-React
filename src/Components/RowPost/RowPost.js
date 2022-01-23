97bc87d021841476d1c1ff024e3cca24c0a91cc9
amalsabu59 <amalsabu59@gmail.com>
2022-01-24 01:08:37 +0530

    css updated

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -23,7 +23,7 @@ function RowPost(props) {
         width: '100%',
         playerVars: {
           // https://developers.google.com/youtube/player_parameters
-          autoplay: 0,
+          autoplay: 1,
         },
     }
     const handleMovie =(id)=>{
